347e6e091ace6a1b7ad649744e1bd731e2bfd872
lyne18-meet <lyneshweiki@gmail.com>
2017-01-17 19:02:42 +0200

    recursion 2nd branch

--- draw_tree.py
+++ draw_tree.py
@@ -6,8 +6,8 @@ length=256 #The length of the first branch
 startx=0
 starty=-250
 branch1=Branch((startx,starty),(startx,starty+length))
+branch2=Branch((startx,starty),(startx,starty+length))
 turtle.hideturtle()
-
 def draw_tree( this_branch, branch_num ):
     '''
     Draw a branch in the tree; then, split the branch and
@@ -17,12 +17,22 @@ def draw_tree( this_branch, branch_num ):
     :param branch_num: the number corresponding to how deep in the tree this branch exists
     '''
     #Complete this method
+    
+    
+    
+    turtle.penup()
+    turtle.goto(this_branch.start)
     turtle.pendown()
-    turtle.goto(0,-256)
+    turtle.goto(this_branch.stop)
 
-    def split(self):
+    branch_list=this_branch.split()
+    draw_tree(branch_list[0],1)
+    draw_tree(branch_list[1],1)
+'''
         rel_branch_size=0.5
     draw_tree(this_branch, branch_num)
+'''
+    
 draw_tree(branch1,1)
 branch1.split()
 
